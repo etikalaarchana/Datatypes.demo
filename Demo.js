17d2296bae02b642c86025f9cde9811a9f0a2ead
etikala archana <archanaetikala51@gmail.com>
2026-02-06 19:26:49 +0530

    fun printed

--- Demo.js
+++ Demo.js
@@ -18,5 +18,12 @@ document.write(a*b) ;
 function div(a,b){
 document.write(a/b) ;
 }
-
-
+function mod(a,b){
+    document.write(a%b)
+}
+add(2,9)
+sub(8,6)
+mul(2,9)
+div(8,2)
+mod(6,3)
+document.write("hllo")
